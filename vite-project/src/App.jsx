8d69970a5ad3bdd65d7d51decdce150990a7e94d
juniorworku <76052158+juniorworku@users.noreply.github.com>
2024-03-01 23:46:41 -0800

    App.css Imported

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import 'semantic-ui-css/semantic.min.css'
 import WeatherIndex from './components/weatherIndex';
 import WeatherCard from './components/weatherCard';
+import './App.css'
 
 function App() {
 
