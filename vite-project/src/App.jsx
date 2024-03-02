4cac4142f354afbdb617a3281b1edab3f069fac5
juniorworku <76052158+juniorworku@users.noreply.github.com>
2024-03-01 23:41:19 -0800

    WeatherCard component Added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import 'semantic-ui-css/semantic.min.css'
 import WeatherIndex from './components/weatherIndex';
+import WeatherCard from './components/weatherCard';
 
 function App() {
 
   return (
     <>
     <WeatherIndex />
+    <WeatherCard />
     </>
   )
 }
