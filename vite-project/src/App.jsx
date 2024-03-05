cbc958557794385c2558dc8d38d94575d88f975b
juniorworku <76052158+juniorworku@users.noreply.github.com>
2024-03-05 05:44:59 -0800

    Adding weather Card Component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import 'semantic-ui-css/semantic.min.css'
 import WeatherIndex from './components/weatherIndex';
 import './App.css'
+import WeatherCard from './components/weatherCard';
 
 function App() {
 
   return (
     <>
     <WeatherIndex />
+    <WeatherCard />
     </>
   )
 }
